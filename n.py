6b23c650b8f3689d1275a63e48a18d2f89151d9a
natch3n <natalieroessel@gmail.com>
2025-01-14 12:22:41 +0100

    corr rotations

--- n.py
+++ n.py
@@ -7,23 +7,23 @@ R_z(theta) = [[e^(-i*theta/2), 0],[0, e^(i*theta/2)]]
 
 # more rotations as Matrices
 # Pi/8 around xy-axis
-R_xy = R_z[-pi/4] * R_x[pi/8] * R_z[pi/4]
+R_xy(theta) = R_z[-pi/4] @ R_x[theta] @ R_z[pi/4]
 # pi/8 around yz-axis
-R_yz = R_x[-pi/4] * R_y[pi/8] * R_x[pi/4]
+R_yz(theta) = R_x[-pi/4] @ R_y[theta] @ R_x[pi/4]
 # pi/8 around xz-axis
-R_xz = R_y[-pi/4] * R_z[pi/8] * R_y[pi/4]
+R_xz(theta) = R_y[-pi/4] @ R_z[theta] @ R_y[pi/4]
 
 # add two qubit gates, first is controll qubit, second is target qubit
-# controlled pi/8 rotation around x axis
-CR_x = np.tensordot(np.array([[1,0],[0,1]]), R_x[pi/8], axes=0)
+# controlled pi rotation around x axis
+CR_x = np.tensordot(np.array([[1,0],[0,1]]), R_x[pi], axes=0)
 # controlled pi/8 rotation around y axis
-CR_y = np.tensordot(np.array([[1,0],[0,1]]), R_y[pi/8], axes=0)
+CR_y = np.tensordot(np.array([[1,0],[0,1]]), R_y[pi], axes=0)
 # controlled pi/8 rotation around z axis
-CR_z = np.tensordot(np.array([[1,0],[0,1]]), R_z[pi/8], axes=0)
+CR_z = np.tensordot(np.array([[1,0],[0,1]]), R_z[pi], axes=0)
 
 # controlled pi/8 rotation around xy axis
-CR_xy = np.tensordot(np.array([[1,0],[0,1]]), R_xy, axes=0)
+CR_xy = np.tensordot(np.array([[1,0],[0,1]]), R_xy[pi], axes=0)
 # controlled pi/8 rotation around yz axis
-CR_yz = np.tensordot(np.array([[1,0],[0,1]]), R_yz, axes=0)
+CR_yz = np.tensordot(np.array([[1,0],[0,1]]), R_yz[pi], axes=0)
 # controlled pi/8 rotation around xz axis
-CR_xz = np.tensordot(np.array([[1,0],[0,1]]), R_xz, axes=0)
+CR_xz = np.tensordot(np.array([[1,0],[0,1]]), R_xz[pi], axes=0)
