dc25b2430ee4ccc4599fcde814c898d766b4ad1a
natch3n <natalieroessel@gmail.com>
2025-01-14 11:53:55 +0100

    controlled rotations

--- n.py
+++ n.py
@@ -13,4 +13,15 @@ R_yz = R_x[-pi/4] * R_y[pi/8] * R_x[pi/4]
 # pi/8 around xz-axis
 R_xz = R_y[-pi/4] * R_z[pi/8] * R_y[pi/4]
 
-# hello I changed stuff pls let me commit
+# add two qubit gates, first is controll qubit, second is target qubit
+# controlled pi/8 rotation around x axis
+CR_x = np.tensordot(np.array([[1,0],[0,1]]), R_x[pi/8], axes=0)
+# controlled pi/8 rotation around y axis
+CR_y = np.tensordot(np.array([[1,0],[0,1]]), R_y[pi/8], axes=0)
+# controlled pi/8 rotation around z axis
+CR_z = np.tensordot(np.array([[1,0],[0,1]]), R_z[pi/8], axes=0)
+
+# controlled pi/8 rotation around xy axis
+CR_xy = np.tensordot(np.array([[1,0],[0,1]]), R_xy, axes=0)
+# controlled pi/8 rotation around yz axis
+CR_yz = np.tensordot(np.array([[1,0],[0,1]]), R_yz, axes=0)
